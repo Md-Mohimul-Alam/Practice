6bd8466a32b3055b262b24c98ccf468710ac7095
MD Mohimul Alam <58184664+Md-Mohimul-Alam@users.noreply.github.com>
2024-06-05 16:21:54 +0600

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+var animation3 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'ad.json'
+});var animation4 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'ad.json'
+});var animation5 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'ad.json'
+});
